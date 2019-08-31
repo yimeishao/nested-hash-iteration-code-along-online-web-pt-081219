d56fd085cb22176dca196c548c1d228d774ce2b9
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 22:52:47 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -1,4 +1,4 @@
-require pry
+require 'pry'
  
   contacts = {
     "Jon Snow" => {
@@ -17,7 +17,9 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
 if person == "Freddy Mercury"
   contact_details_hash.each do |attribute, data|
-    binding.pry
+if attribute == :favorite_ice_cream_flavors
+  data.delete_if {|ice_cream| ice_cream == "strawberry"}
 end
+  end
 end
 end
