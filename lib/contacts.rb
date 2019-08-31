f3647f957a295dab967736ad13584ee440e28be2
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-31 22:51:21 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,6 +17,7 @@ def remove_strawberry(contacts)
 contacts.each do |person, contact_details_hash|
 if person == "Freddy Mercury"
   contact_details_hash.each do |attribute, data|
+    binding.pry
 end
 end
 end
